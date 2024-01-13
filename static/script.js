fb0ba2282695ed4dd2795a4a4018d60665902666
kulbir singh <kulbir2406@gmail.com>
2024-01-12 22:46:34 -0700

    added styling to the main page

---
+++ static/script.js
@@ -0,0 +1,24 @@
+function displayImage() {
+  var input = document.getElementById("image");
+  var image = document.getElementById("uploadedImage");
+  var bgImage = document.getElementById("uploadedBGImage");
+  let cancelBtn = document.getElementById("cancelButton");
+
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      image.src = e.target.result;
+      bgImage.src = e.target.result;
+      bgImage.style.opacity = "20%";
+      bgImage.style.filter = "blur(10px)";
+      cancelBtn.style.display = "inline";
+    };
+    cancelBtn.onclick = function () {
+      image.src = "";
+      bgImage.src = "";
+      cancelBtn.style.display = "none";
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
