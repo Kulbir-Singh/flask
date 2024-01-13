76f90288f6b5ee1c8265d149d8293df0281d3471
kulbir singh <kulbir2406@gmail.com>
2024-01-12 20:02:10 -0700

    removed bugs and extra code

--- main.py
+++ main.py
@@ -9,27 +9,8 @@ app = Flask(__name__)
 current_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..' ))
 # os.path.dirname(os.path.abspath(__file__))
 
-input_file_path = current_dir + '/remove-bg/images/john.PNG'
-print("Current script directory:", current_dir + '/images/terry-rozier.jpg')
-
-def remove_background(input_path, output_path):
-  input = Image.open(input_path)
-  output = remove(input)
-  output.save(output_path)
- 
-def process_image(input_path, output_path):
-    image = Image.open(input_path)
-    grayscale_image = image.convert("L")
-    grayscale_image.save(output_path)
-    print(f"Image processed and saved to {output_path}")
-
 @app.route('/')
-def index():
-  remove_background(input_file_path, "output_grayscale.png")
-  image_path = current_dir + '/remove-bg/output_grayscale.png'
-  
-  mime_type = 'image/png'
-    
+def index(): 
   return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
